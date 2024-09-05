cda0f7d9d20bf68234b47b69976ae58a7d880710
Dominik Dolejší <bukefalus007@gmail.com>
2024-09-05 11:40:35 +0200

    added github actions and formated thing

--- middleware/logger.ts
+++ middleware/logger.ts
@@ -2,18 +2,18 @@ import { Middleware } from "@oak/oak";
 import * as colors from "@std/fmt/colors";
 
 export const logger: Middleware = async (ctx, next) => {
-    await next();
-    const c = ctx.response.status >= 500
-        ? colors.red
-        : ctx.response.status >= 400
-        ? colors.yellow
-        : colors.green;
-    console.log(
-        `${c(ctx.request.method)} ${c(`(${ctx.response.status})`)} - ${
-            colors.rgb8(
-                `${ctx.request.url.pathname}${ctx.request.url.search}`,
-                72,
-            )
-        }`,
-    );
+  await next();
+  const c = ctx.response.status >= 500
+    ? colors.red
+    : ctx.response.status >= 400
+    ? colors.yellow
+    : colors.green;
+  console.log(
+    `${c(ctx.request.method)} ${c(`(${ctx.response.status})`)} - ${
+      colors.rgb8(
+        `${ctx.request.url.pathname}${ctx.request.url.search}`,
+        72,
+      )
+    }`,
+  );
 };
