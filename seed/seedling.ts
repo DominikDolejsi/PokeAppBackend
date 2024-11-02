59e6b5c121f1beb0f70170c421caa8b380050e0b
Dominik Dolejší <bukefalus007@gmail.com>
2024-11-02 18:27:38 +0100

    fixed seedling to await

--- seed/seedling.ts
+++ seed/seedling.ts
@@ -3,14 +3,9 @@ import { PokemonModel } from "../api/v1/pokemon/pokemon.model.ts";
 import file from "./pokemon.json" with { type: "json" };
 
 const seedDB = async () => {
-    const currentDB = await PokemonModel.find();
-    if (currentDB) {
-        console.log("Found some documents in DB");
-        PokemonModel.deleteMany();
-        console.log("Cleard DB");
-    }
-
-    const createdPokemon = await PokemonModel.insertMany(file);
+    await PokemonModel.find();
+    await PokemonModel.deleteMany();
+    await PokemonModel.insertMany(file);
 };
 
 export const seedlingRouter = new Router({
